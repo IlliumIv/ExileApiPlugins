72aa87380bc0fab5765192884186c55ae82cbdf9
IlliumIv <alm.sparta.ao@gmail.com>
2019-10-01 18:05:32 +0500

    Added CheatSheets plugin

---
+++ CheatSheets/Leagues/Incursion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheatSheets.Leagues
+{
+    public class Incursion : Sheet
+    {
+        public Incursion()
+        {
+            Name = "Incursion";
+
+            Preloads = new List<string>
+            {
+                "Metadata/NPC/League/Incursion/TreasureHunterWild"
+            };
+
+            // Sheets.Preloads.Add(Name, Preloads);
+        }
+    }
+}
---
+++ CheatSheets/Sheet.cs
@@ -0,0 +1,15 @@
+﻿using CheatSheets.Leagues;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheatSheets
+{
+    public class Sheet
+    {
+        public string Name { get; protected set; }
+        public List<string> Preloads { get; protected set; }
+    }
+}
