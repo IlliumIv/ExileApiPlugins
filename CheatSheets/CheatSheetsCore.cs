f13d75256575d80f4a4e4f8979abd305a72c9fa0
IlliumIv <alm.sparta.ao@gmail.com>
2019-10-03 08:58:45 +0500

    Created texture atlas

--- CheatSheets/CheatSheetsCore.cs
+++ CheatSheets/CheatSheetsCore.cs
@@ -14,7 +14,7 @@ using System.Windows.Forms;
 
 namespace CheatSheets
 {
-    public class CheatSheetsCore : BaseSettingsPlugin<Settings>
+    public partial class CheatSheetsCore : BaseSettingsPlugin<Settings>
     {
         private bool isLoading;
         private bool working;
@@ -38,6 +38,8 @@ namespace CheatSheets
         public override bool Initialise()
         {
             Name = "CheatSheets";
+
+            InitialiseTexture();
 
             debugInformation = new DebugInformation("Preload parsing", false);
 
---
+++ CheatSheets/Textures/Textures.cs
@@ -0,0 +1,35 @@
+﻿using ExileCore.Shared.AtlasHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheatSheets
+{
+    partial class CheatSheetsCore
+    {
+        private AtlasTexture _iconIncursion;
+        private AtlasTexture _iconBetrayal;
+        private AtlasTexture _iconDelve;
+        private AtlasTexture _iconUnknown;
+
+        public bool InitialiseTexture()
+        {
+            try
+            {
+                _iconIncursion = GetAtlasTexture("IconIncursion");
+                _iconBetrayal = GetAtlasTexture("IconBetrayal");
+                _iconDelve = GetAtlasTexture("IconDelve");
+                _iconUnknown = GetAtlasTexture("IconUnknown");
+            }
+            catch(Exception e)
+            {
+                LogMessage(e.Message);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
